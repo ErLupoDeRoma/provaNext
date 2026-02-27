2d37b723f884ad91b68fe2afe4c765e95b16d96f
Luca Rivoli <l.rivoli@donboscosandona.it>
2026-02-27 09:25:23 +0100

    dovrebbe andare

---
+++ prova/app/Contatti/page.js
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Contatti() {
+  return (
+    <h1>Ciao a tutti, da pagina contatti</h1>
+  );
+}
---
+++ prova/app/Prodotti/page.js
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Prodotti() {
+  return (
+    <h1>Ciao a tutti, da pagina prodotti</h1>
+  );
+}
