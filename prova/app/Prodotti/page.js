0494b6c298e80ac3f7fb7001aa92da9d17b5812e
Luca Rivoli <l.rivoli@donboscosandona.it>
2026-02-27 10:08:07 +0100

    27/02/2026-10:08

--- prova/app/Prodotti/page.js
+++ prova/app/Prodotti/page.js
@@ -1,7 +1,11 @@
 import Image from "next/image";
-
+import Link
+ from "next/link";
 export default function Prodotti() {
   return (
+    <>
     <h1>Ciao a tutti, da pagina prodotti</h1>
+    <Link href={"/"} className="bg-red-800 p-5 border-1 rounded-3xl block mt-4 ml-10 w-30 text-center">Home Page</Link>
+</>
   );
 }
--- prova/app/page.js
+++ prova/app/page.js
@@ -1,7 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
+    <>
+    <h1>Home</h1>
     <h1>Ciao a tutti</h1>
+    <a href="Prodotti">Prodotti</a>
+    
+    </>
   );
 }
